9c16eb8d12459caa47c74410275c4b927fe4df08
lachlamp <lachlan.potter@uqconnect.edu.au>
2025-10-31 09:02:22 +1000

    Added Matplot visualiser to predict.py to see input vs visualiser vs ground truth.:

--- recognition/2DOasis-UNET_48011183/predict.py
+++ recognition/2DOasis-UNET_48011183/predict.py
@@ -9,18 +9,22 @@ device = "mps" if torch.backends.mps.is_available() else "cpu"
 
 images = "OASIS/keras_png_slices_test"
 masks  = "OASIS/keras_png_slices_seg_test"
+# Process one image at a time for prediction
 BATCH_SIZE = 1
 
+# Load test dataset and create DataLoader
 dataset = OASISDataset(images, masks)
 test_loader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=False)
 
-
+# Path to model checkpoint
 MODEL_PATH = "checkpoints/improved_unet_epoch1.pth"
+# Initialise model and load saved weights
 model = ImprovedUNET(channels=1, classes=1, bilinear=True).to(device)
 model.load_state_dict(torch.load(MODEL_PATH, map_location=device))
+# Evaluation mode
 model.eval()
 
-
+# Used to avoid division by zero
 EPSILON = 1e-5
 def dice(prediction, target):
     """
@@ -61,11 +65,43 @@ with torch.no_grad():
         mask = mask.to(device)
 
         output = model(image)
+        # COnvert logits to probabilities
         pred_mask = torch.sigmoid(output)
+        # Threshold to get binary mask
         pred_mask_bin = (pred_mask > 0.5).float()
 
+        # Save to disk
         plt.imsave(f"predicted_masks/mask_{i}.png", pred_mask_bin[0,0].cpu(), cmap='gray')
 
+        # Computes dice score for each image, and stored to calculate average
         dice_score = dice(output, mask)
         dice_scores.append(dice_score)
         print(f"Image {i}: Dice = {dice_score:.4f}")
+
+        # Visualise first 5 images, predicted masks, and ground truth
+        if i < 5:
+            fig, axs = plt.subplots(1, 3, figsize=(12, 4))
+            # Input
+            axs[0].imshow(image[0,0].cpu(), cmap='gray')
+            axs[0].set_title("Input Image")
+            axs[0].axis('off')
+
+            # Predicted mask
+            axs[1].imshow(pred_mask_bin[0,0].cpu(), cmap='gray')
+            axs[1].set_title("Predicted Mask")
+            axs[1].axis('off')
+
+            # Ground truth mask
+            axs[2].imshow(mask[0,0].cpu(), cmap='gray')
+            axs[2].set_title("Ground Truth Mask")
+            axs[2].axis('off')
+
+            # Display without blocking execution
+            plt.show(block=False)  
+            plt.pause(0.5)         
+            plt.close()            
+
+# Calculates, prints the overall test set Dice coefficient as metric for model
+# performance.
+average_dice = sum(dice_scores) / len(dice_scores)
+print(f"\nAverage Test Dice: {average_dice:.4f}")
